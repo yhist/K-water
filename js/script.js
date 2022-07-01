ed03af975dd29bf1f1ee3d00a67d49e9cdd0a1ac
yhist <yhyh0903@gmail.com>
2022-07-01 17:27:53 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+});
+window.onload = function(){
+  // 메뉴기능
+  let header = $('.header');
+  let gnb = $('.gnb');
+  let gnbH = gnb.height();
+  console.log(gnbH);
+  gnb.mouseenter(function(){
+    header.css('height',  gnbH);
+  });
+  gnb.mouseleave(function(){
+    header.css('height', 70)
+  });
+  // 비주얼 슬라이드
+  new Swiper('.sw-visual', {
+    slidesPerView: 8,
+    loop: true,
+    navigation: {
+      prevEl: '.sw-visual-prev',
+      nextEl: '.sw-visual-next'
+    }
+  });
+};
